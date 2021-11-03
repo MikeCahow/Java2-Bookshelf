c5314e95e27edc6bb1dac881625536894e33dc37
Allie Murphy <allie-murphy@student.kirkwood.edu>
2021-11-03 13:28:19 -0500

    Add ChildrensBook class

--- src/com/cahow/Main.java
+++ src/com/cahow/Main.java
@@ -1,8 +1,11 @@
 package com.cahow;
 
+import java.awt.print.Book;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
     }
+    Book b4 = new ChildrensBook("Java for Kids", "Allie", "Mike");
 }
